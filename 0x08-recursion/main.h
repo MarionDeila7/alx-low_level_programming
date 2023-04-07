cfe0145ab3b4d20738d707a55d68fb974a89366a
MarionDeila7 <mariondeila7@gmail.com>
2023-04-07 07:23:52 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MIN_H
+
+void_puts_recursion(char *s);
+int_putchar(char c);
+void_print_rev_recursion(char *s);
+int_strlen_recursion(char *s);
+int factorial(int n);
+int_pow_recursion(int n);
+int_sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+#endif
